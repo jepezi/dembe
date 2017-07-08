625dc0122b63e01f33ba1e0239f33c6fd050d791
Jirat Arinrith <jepezi@gmail.com>
2017-07-08 21:29:43 +0700

    HMR

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -14,15 +14,29 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(paths.root, 'public', 'build'),
-    publicPath: '/'
+    publicPath: 'http://localhost:8001/',
   },
   module: {
     rules: [
       ruleJS.dev,
     ]
   },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: false,
+      debug: true
+    }),
+
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ],
   devServer: {
     port: 8001,
-    inline: true
+    inline: true,
+    hotOnly: true,
+    headers: { 'Access-Control-Allow-Origin': 'http://localhost:8000' },
+    noInfo: true,
+    historyApiFallback: true,
+    compress: true,
   }
 }
--- web/index.js
+++ web/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function App() {
-  return (
-    <div>Hello React</div>
-  )
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello React</div>
+    )
+  }
 }
 
 ReactDOM.render(
