07df511832c615c1185b08ec2639d7a869382ecf
Jirat Arinrith <jepezi@gmail.com>
2017-07-22 15:41:54 +0700

    Cache renderer on server with memory-cache

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const compression = require('compression')
 const proxy = require('http-proxy-middleware')
 const renderer = require('./renderer')
 const paths = require('../config/paths')
+const cache = require('./cache')
 
 const app = express()
 if (process.env.NODE_ENV === 'production') {
@@ -14,7 +15,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(favicon(path.resolve(paths.public, 'favicon.ico')))
 app.use('/api', proxy({target: 'http://localhost:3000', changeOrigin: true}))
 app.use(express.static(paths.public))
-app.use(renderer)
+app.use(cache(10), renderer)
 app.listen(8000, () => {
   console.warn('running at http://localhost:8000')
 })
